5d207d4ab2caa1fb9b16b0c47acf6ce547033eed
Yaks Souza <pl4nk1ton@gmail.com>
2023-01-31 17:14:10 -0300

    fix: fix swagger documentation for Post endpoint

--- pkg/newsletter/handler/post.go
+++ pkg/newsletter/handler/post.go
@@ -5,15 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Get defines GET /newsletter/subscriptions endpoint
-// @Summary      Read subscriptions
+// Get defines GET /newsletter/subscription endpoint
+// @Summary      Create subscriptions
 // @Tags         subscriptions
-// @Router       /newsletter/subscriptions [get]
-// @Param        page	        query  int		 true   "Result page"                                   example(1)
-// @Param        maxPageSize	query  int		 true   "Max page size"                                  example(10)
-// @Param        userId	        query  string	 false  "User ID"                                        example(c3d96267-9f4e-4980-95a1-b7080348d456)
-// @Param        blogId	        query  string	 false  "Blog ID"                                        example(6d52cf12-84c0-4d6b-a3a1-cf6350362590)
-// @Param        interests	    query  []string  false  "Interests"                                      example(["tech","sports"])
+// @Router       /newsletter/subscriptions [post]
+// @Param        sub body newsletter.Subscription true "Subscription"
 // @Produce      json
 // @Success      200
 // nolint:gocyclo //error checking branches
